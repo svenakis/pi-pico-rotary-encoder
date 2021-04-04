6eadb7a2a1d66d2e7ad681a9979fdc61f9e1d1af
svenakis <sven-ba@online.no>
2021-04-04 23:07:49 +0200

    Create source.h

---
+++ source.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/irq.h"
+#include "hardware/gpio.h"
+#include "pico/time.h"
+#include "pico.h"
+#include "hardware/structs/i2c.h"
+#include "pico/binary_info.h"
+#include "pico/bootrom.h"
